1c08dfd47f6355aa66be59b6b8012c4eee894a3a
PieroVV <alexis.vasquezv@pucp.edu.pe>
2023-08-17 21:06:59 -0500

    Segundo cambio

--- INF237_2023-1_D1_06M1.cpp
+++ INF237_2023-1_D1_06M1.cpp
@@ -1,14 +1,8 @@
-// INF237_2023-1_D1_06M1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+?
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.7.34009.444
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "INF237_2023-1_D1_06M1", "INF237_2023-1_D1_06M1.vcxproj", "{6580FA23-DDDF-44E5-A454-44CE8127F950}"
+EndProject
+Global
