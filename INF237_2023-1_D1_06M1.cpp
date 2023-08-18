3de4786fc20d6f4353b10da796c8ef008590b97d
kjhosep06 <a20172735@pucp.edu.pe>
2023-08-17 21:05:12 -0500

    segundo cambio de LPOO

--- INF237_2023-1_D1_06M1.cpp
+++ INF237_2023-1_D1_06M1.cpp
@@ -2,9 +2,38 @@
 
 using namespace std;
 
-int mayor(int arrNum[15], int n); //comentario
-void Fibonacci(int fibonacci[30]);
-void orden(int fibonacci[30], char arrWord[15], int arrNum[15], int n, int mayor);
+?
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.7.34009.444
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "INF237_2023-1_D1_06M1", "INF237_2023-1_D1_06M1.vcxproj", "{6580FA23-DDDF-44E5-A454-44CE8127F950}"
+EndProject
+Global
+GlobalSection(SolutionConfigurationPlatforms) = preSolution
+Debug | x64 = Debug | x64
+Debug | x86 = Debug | x86
+Release | x64 = Release | x64
+Release | x86 = Release | x86
+EndGlobalSection
+GlobalSection(ProjectConfigurationPlatforms) = postSolution
+{ 6580FA23 - DDDF - 44E5 - A454 - 44CE8127F950 }.Debug | x64.ActiveCfg = Debug | x64
+{ 6580FA23 - DDDF - 44E5 - A454 - 44CE8127F950 }.Debug | x64.Build.0 = Debug | x64
+{ 6580FA23 - DDDF - 44E5 - A454 - 44CE8127F950 }.Debug | x86.ActiveCfg = Debug | Win32
+{ 6580FA23 - DDDF - 44E5 - A454 - 44CE8127F950 }.Debug | x86.Build.0 = Debug | Win32
+{ 6580FA23 - DDDF - 44E5 - A454 - 44CE8127F950 }.Release | x64.ActiveCfg = Release | x64
+{ 6580FA23 - DDDF - 44E5 - A454 - 44CE8127F950 }.Release | x64.Build.0 = Release | x64
+{ 6580FA23 - DDDF - 44E5 - A454 - 44CE8127F950 }.Release | x86.ActiveCfg = Release | Win32
+{ 6580FA23 - DDDF - 44E5 - A454 - 44CE8127F950 }.Release | x86.Build.0 = Release | Win32
+EndGlobalSection
+GlobalSection(SolutionProperties) = preSolution
+HideSolutionNode = FALSE
+EndGlobalSection
+GlobalSection(ExtensibilityGlobals) = postSolution
+SolutionGuid = { C1FC1092 - 3D97 - 4D46 - 8F5D - 2E242F2984F5 }
+EndGlobalSection
+EndGlobal
+
 
 int main(int argc, char** argv) {
 	int n, i, num, Mayor;
