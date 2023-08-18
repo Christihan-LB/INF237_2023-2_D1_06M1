fb614f3fdbf8be4d1e8b05df40718a874bfaa4fe
frankerickna <a20203824@pucp.edu.pe>
2023-08-17 20:56:46 -0500

    askhskjdfdsjn ya

--- INF237_2023-1_D1_06M1.cpp
+++ INF237_2023-1_D1_06M1.cpp
@@ -30,47 +30,4 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-int mayor(int arrNum[15], int n) {
-	int i = 0, mayor = 0;
-	while (i < n) {
-		if (arrNum[i] > mayor) {
-
-			mayor = arrNum[i];
-		}
-		i++;
-	}
-	return mayor;
-}
-
-void Fibonacci(int fibonacci[30]) {
-	fibonacci[0] = 1;
-	fibonacci[1] = 2;
-	for (int i = 2; i < 30; i++) {
-		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-	}
-
-}
-void orden(int fibonacci[30], char arrWord[15], int arrNum[15], int n, int mayor) {
-	int i = 0, j = 0;
-	bool stop = false, found = false;
-	cout << "La palabra decodificada es: ";
-	while (!stop) {
-
-		while (i < n) {
-			if (fibonacci[j] == arrNum[i]) {
-				cout << arrWord[i];
-				found = true;
-			}
-			i++;
-			if (i == n && found == false) {
-				cout << " ";
-			}
-		}
-		if (fibonacci[j] == mayor) {
-			stop = true;
-		}
-		j++;
-		i = 0;
-		found = false;
-	}
-}
+//xddddddddddddddddddd
