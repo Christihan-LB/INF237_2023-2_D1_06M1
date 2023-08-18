36d974aad9e01369268c80b9e89dbd5bc3297f63
AmirYisusFE <a20201315@pucp.edu.pe>
2023-08-17 21:03:41 -0500

    ta p0tente

--- INF237_2023-1_D1_06M1.cpp
+++ INF237_2023-1_D1_06M1.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
 }
-
+// ta p0tente
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
